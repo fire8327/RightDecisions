2d243fbbe6c6164daa844f94173513f8c00000ae
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-09-09 01:06:17 +0300

    create header

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -2,7 +2,12 @@
 /* navigation */
 $("#toggler").click(()=>{
   $("#menu").toggleClass("max-lg:-translate-y-full")
-  $("#menu").toggleClass("top-0")
-  $("#menu").toggleClass("top-full")
+  $("#menu").toggleClass("max-lg:top-0")
+  $("#menu").toggleClass("max-lg:top-full")
+  $("#toggler").toggleClass("h-5")
+  $("#toggler > :nth-child(1)").toggleClass("rotate-45")
+  $("#toggler > :nth-child(2)").toggleClass("opacity-0")
+  $("#toggler > :nth-child(3)").toggleClass("-rotate-45")
   $("body").toggleClass("overflow-hidden")
+  $("#overlay").toggleClass("hidden")
 })
