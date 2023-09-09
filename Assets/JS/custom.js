3ae8bb4a827743ab29db4b617ea5a9feac8d64b9
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-09-09 16:11:42 +0300

    create js&validate

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -11,3 +11,49 @@ $("#toggler").click(()=>{
   $("body").toggleClass("overflow-hidden")
   $("#overlay").toggleClass("hidden")
 })
+
+/* links */
+$('a[href^="#"]').on('click', function() {
+  $("body").removeClass("overflow-hidden")
+  let href = $(this).attr('href')
+    $('html, body').animate({
+        scrollTop: $(href).offset().top - 0
+    })
+  return false
+});
+
+/* validate */
+const validate = new JustValidate('#form', {
+  validateBeforeSubmitting: true,
+});
+function Validate() {
+  validate
+  .addField('#number', [
+      {
+          rule: 'required',
+          errorMessage: 'Поле Номер телефона обязательно к заполнению',
+      }  ,    
+      {
+          rule: 'customRegexp',
+          value: /[0-9]/,      
+          errorMessage: 'Поле Номер телефона должно содержать только цифры',
+      },
+      {
+        rule: 'minLength',
+        value: 11,      
+        errorMessage: 'Поле Номер телефона должно иметь минимум 11 символов',
+      }
+  ])
+  .addField('#name', [
+      {
+          rule: 'required',
+          errorMessage: 'Поле Имя обязательно к заполнению',
+      },
+      {
+        rule: 'minLength',
+        value: 2,      
+        errorMessage: 'Поле Имя должно иметь минимум 2 символа',
+      }
+  ]);
+}
+Validate()
